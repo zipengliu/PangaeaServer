4fe5ab9dc3ad417651c279d26e8e4b7d53f042fb
Zipeng Liu <zipeng@cs.ubc.ca>
2016-10-28 14:03:02 -0700

    setup

---
+++ README.md
@@ -0,0 +1,3 @@
+Backend Server
+
+Start the server with `node app.js`.
---
+++ app.js
@@ -0,0 +1,10 @@
+ivar express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+      res.send('Hello World!');
+});
+
+app.listen(6666, function () {
+      console.log('Example app listening on port 6666!');
+});
